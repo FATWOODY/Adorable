4822b027790b114f1cd00fdd7060c2a8154c73a1
Michael <154087850+FATWOODY@users.noreply.github.com>
2025-08-01 10:48:26 -0500

    Fix Headers Context by Forcing Dynamic Rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { getUserApps } from "@/actions/user-apps";
 import { HomeClient } from "@/components/home-client";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const initialUserApps = await getUserApps();
   
